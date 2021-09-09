2f80948c62a7e56c997c415fa3f76c0d6b6b3264
appym005 <apoorvmishra05@gmail.com>
2021-09-09 22:59:54 +0530

    executor update

--- executor.py
+++ executor.py
@@ -1,13 +1,21 @@
 import subprocess
-import os 
+
 
 def execute(c):
-	s = list(c.split(' '))
+	a = list(c.split('|'))
+	print(a)
+	s = [i.split() for i in a]
 	print(s)
-	s = subprocess.run(s,capture_output=True)
-	r = f'\nstdout:\n{s.stdout.decode()}\n\nstderr:\n{s.stderr.decode()}'
-	print(s.stdout)
-	print(s.stderr)
+	s1 = subprocess.run(s[0], universal_newlines=True, stdout=subprocess.PIPE)
+	if '|' in c:
+		s2 = subprocess.run(s[1], universal_newlines=True, input=s1.stdout, stdout=subprocess.PIPE)
+		r = f'\nstdout:\n{s2.stdout}\n\nstderr:\n{s2.stderr}'
+		print(s2.stdout)
+		print(s2.stderr)
+		return r
+	r = f'\nstdout:\n{s1.stdout}\n\nstderr:\n{s1.stderr}'
+	print(s1.stdout)
+	print(s1.stderr)
 	return r
 
-#execute('ls')
+#print(execute('echo hello > zxc'))
